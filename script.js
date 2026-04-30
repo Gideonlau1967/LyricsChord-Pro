a00f876a5909d56983ae044b24ef6694e0727006
Gideonlau1967 <gideonlau@gmail.com>
2026-04-30 15:04:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- CONSTANTS & VERSION ---
-    const VERSION = "1.6.2-PRO-FINAL";
+    const VERSION = "Version 2.0";
     const MAIN_FONT = "Times New Roman";
     const SIZE_TITLE = 32, SIZE_LYRIC = 24, SIZE_CHORD = 14, SIZE_SECTION = 16, SIZE_COPY = 14;
     const PT_TO_PX = 96 / 72; 
@@ -126,7 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPreviewIndex = 0;
             document.getElementById('keyShift').innerText = "Shift: 0";
             updatePreview();
-            alert("Smart Import Successful!");
+            // alert("Smart Import Successful!");
 
         } catch (err) {
             console.error(err);
